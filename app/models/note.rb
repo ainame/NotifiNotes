d4e78316fbcc434a27dbdfe18e9895e02ff093e5
ainame <s.namai.09@gmail.com>
2014-10-27 03:13:29 +0900

    add after_create hook

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,9 @@
 class Note < ActiveRecord::Base
+  after_create :send_notification
+
+  def send_notification
+    topic = Notification::Topic::Note.new(self)
+    event = Notification::Event::Note::Post.new(self)
+    topic.post_event(self.user, event)
+  end
 end
