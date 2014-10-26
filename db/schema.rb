b4eb70fd1c44bc956e8260e183fc9b40d7d569d2
ainame <s.namai.09@gmail.com>
2014-10-27 04:44:36 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026073001) do
+ActiveRecord::Schema.define(version: 20141026075308) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141026073001) do
   create_table "endpoints", force: true do |t|
     t.integer  "user_id"
     t.integer  "platform"
+    t.text     "device_token"
     t.string   "arn"
     t.datetime "created_at"
     t.datetime "updated_at"
